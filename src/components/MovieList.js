960e19aefdda2bf9e8a38f6419d716d1cd07fd76
Ahmadshah Wahidy <shah.wahidy@gmail.com>
2022-01-18 16:29:08 -0800

    completed module

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import {connect} from 'react-redux'
 import MovieListItem from './MovieListItem';
 import MovieFooter from './MovieFooter';
 
 const MovieList = (props)=> {
-    const movies = [];
+    //console.log(props)
+    const {movies} = props;
 
     return (
         <div className="col">
@@ -31,4 +32,9 @@ const MovieList = (props)=> {
     );
 }
 
-export default MovieList;
+const mapStateToProps = (state) => {
+    return({
+        movies: state.movieReducer.movies,
+    })
+}
+export default connect(mapStateToProps)(MovieList);
